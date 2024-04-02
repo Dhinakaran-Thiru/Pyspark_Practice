086ceb95677240b28f928dea34874a94b277f14d
Dhinakaran <dhina15003@gmail.com>
2024-04-02 13:18:57 +0530

    commited union and union all using distinct and drop duplicates

--- DATA_FRAME/union and union all.py
+++ DATA_FRAME/union and union all.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+'''from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField,IntegerType,StringType
 Spark=SparkSession.builder.appName("union and union all").getOrCreate()
 data=[("dhina",56,"data engineer",123456),
@@ -20,4 +20,32 @@ df2=Spark.createDataFrame(data=data1,schema=Schema)
 df2.show()
 df1.union(df2).distinct().show()
 #union() and union all() have same functionalities add the duplicate rows also
-#by using distinct() we avoid duplicates
+#by using distinct() we avoid duplicates'''
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+# Prepare data
+data = [("James", "Sales", 3000),
+    ("Michael", "Sales", 4600),
+    ("Robert", "Sales", 4600),
+    ("James", "Sales", 3000)
+  ]
+columns= ["employee_name", "department", "salary"]
+
+# Create DataFrame
+df = spark.createDataFrame(data = data, schema = columns)
+df.printSchema()
+df.show(truncate=False)
+
+# Using distinct()
+distinctDF = df.distinct()
+distinctDF.show(truncate=False)
+
+# Using dropDuplicates()
+dropDisDF = df.dropDuplicates(["department","salary"])
+dropDisDF.show(truncate=False)
+
+# Using dropDuplicates() on single column
+dropDisDF = df.dropDuplicates(["salary"]).select("salary")
+dropDisDF.show(truncate=False)
+print(dropDisDF.collect())
